eeec2aec559a3dc50d286aa4b1b7c64fe6d16cd0
Storm dragon <stormdragon2976@gmail.com>
2016-02-10 14:49:02 -0500

    Improved xinitrc generation.

--- setup.sh
+++ setup.sh
@@ -63,6 +63,8 @@ if [ -d /etc/X11/xinit/xinitrc.d ]; then
   unset f
 fi
 export GTK_MODULES=gail:atk-bridge
+export GNOME_ACCESSIBILITY=1
+export QT_ACCESSIBILITY=1
 
 exec ratpoison' > $HOME/.xinitrc
 }
